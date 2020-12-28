843041cd8af9a5d16bf8f9028015bda6dfbb7897
azhar <azhar98625aug@gmail.com>
2020-12-28 18:34:27 +0530

    Fourth Commit

--- src/test/java/browertests/EdgeBrowserTest.java
+++ src/test/java/browertests/EdgeBrowserTest.java
@@ -3,20 +3,22 @@ package browertests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
 
 public class EdgeBrowserTest {
 	
 	private static WebDriver driver;
 	
-	public static void main(String[] args) {
+	@Test
+	public void testcase2() throws InterruptedException {
 		System.setProperty("webdriver.edge.driver", System.getProperty("user.dir") + "/drivers/" + "msedgedriver.exe");
 		driver = new EdgeDriver();
-		driver.get("https://google.com");
 		driver.manage().window().maximize();
+		driver.get("https://google.com");
 		driver.findElement(By.name("q")).sendKeys("Azhar");
+		Thread.sleep(5000);
 		driver.quit();
 	}
-	
 	
 
 }
